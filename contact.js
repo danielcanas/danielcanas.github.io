9f07486d22b311e4eb7cec6d599a4e5f5250fbe9
danielcanas <danielcanas10@knights.ucf.edu>
2016-12-24 03:46:54 -0500

    Commit

--- contact.js
+++ contact.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
-
-  //Helllo
   $('#contact-form').submit(function(e) {
     var name    = document.getElementById('name');
     var email   = document.getElementById('email');
-    var message = document.getElementById('comment');
+    var message = document.getElementById('message');
 
-        $.ajax({
+      else {
+      $.ajax({
         method: 'POST',
         url: '//formspree.io/danielcanas10@gmail.com',
         data: $('#contact-form').serialize(),
         datatype: 'json'
       });
-    });
+      e.preventDefault();
+      $(this).get(0).reset();
+    }
   });
+});
