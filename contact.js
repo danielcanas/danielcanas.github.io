556997a83780634a6149eadf6aab9d182d715f4d
danielcanas <danielcanas10@knights.ucf.edu>
2016-12-24 04:13:24 -0500

    Commit

--- contact.js
+++ contact.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   $('#contact-form').submit(function(e) {
     var name = document.getElementById('name');
     var email = document.getElementById('email');
@@ -15,3 +15,18 @@ $(document).ready(function() {
 
   });
 });
+
+/*<form id="contact-form">
+
+  <label for="Name" id="name-label">Name/Company:</label>
+  <input name="name" type="text" class="form-control" id="name">
+
+  <label for="Email" id="email-label">Email:</label>
+  <input name:"email" type="email" class="form-control" id="email">
+
+  <label for="Message" id="message-label">Message:</label>
+  <textarea name:"message" class="form-control contact-input" rows="5" id="message"></textarea>
+
+  <input id="submit" name="submit" type="submit" value="Send">
+
+</form>
