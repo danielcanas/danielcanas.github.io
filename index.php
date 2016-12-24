9b7361a277602907261212f49f5cf1eea7925bdf
danielcanas <danielcanas10@knights.ucf.edu>
2016-12-23 22:52:59 -0500

    Commit

--- index.php
+++ index.php
@@ -1,35 +1,12 @@
 <?php
 
-$EmailFrom = "chriscoyier@gmail.com";
-$EmailTo = "danielcanas10@gmail.com";
-$Subject = "Nice & Simple Contact Form by CSS-Tricks";
-$Name = Trim(stripslashes($_POST['name']));
-$Email = Trim(stripslashes($_POST['email']));
-$Message = Trim(stripslashes($_POST['message']));
+  $name = $_POST['name'];
+  $email= $_POST['email'];
+  $message = $_POST['message'];
+  $to = "danielcanas10@knights.ucf.edu";
+  $subject = "New Message";
 
-// validation
-$validationOK=true;
-if (!$validationOK) {
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
-  exit;
-}
-
-// prepare email body text
-$Body = "";
-$Body .= "Name: ";
-$Body .= $Name;
-$Body .= "\n";
-$Body .= "Tel: ";
-$Body .= $Tel;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $Email;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $Message;
-$Body .= "\n";
-
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+  mail($to, $subject, $message, "From: " . $name);
+  echo "Your message has been sent";
 
 ?>
