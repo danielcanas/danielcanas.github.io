ab1617dbfce1f7dacf7aa76c932de4e81188e495
danielcanas <danielcanas10@knights.ucf.edu>
2016-12-23 22:56:37 -0500

    Commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   $name = $_POST['name'];
   $email= $_POST['email'];
   $message = $_POST['message'];
-  $to = "danielcanas10@knights.ucf.edu";
+  $to = "danielcanas10@gmail.com";
   $subject = "New Message";
 
   mail($to, $subject, $message, "From: " . $name);
