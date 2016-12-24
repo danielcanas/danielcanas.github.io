a4ffb6dd0c4606bb64abf4cb734f2e30394809ba
danielcanas <danielcanas10@knights.ucf.edu>
2016-12-23 21:27:33 -0500

    Commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+$EmailFrom = "chriscoyier@gmail.com";
+$EmailTo = "danielcanas10@gmail.com";
+$Subject = "Nice & Simple Contact Form by CSS-Tricks";
+$Name = Trim(stripslashes($_POST['name']));
+$Email = Trim(stripslashes($_POST['email']));
+$Message = Trim(stripslashes($_POST['message']));
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Tel: ";
+$Body .= $Tel;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+?>
