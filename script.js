a7888281edae6ceaeb594294209b6abf1c587c84
danielcanas <danielcanas10@knights.ucf.edu>
2016-12-25 18:19:29 -0500

    Commit

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ $(document).ready(function(){
 
   $("#form").hide();
 
+  $(".element").typed({
+        strings: ["Student.", "Developer.", "Programmer."],
+        typeSpeed: 60,
+        backspeed: 60,
+        loop: true
+      });
+
   $("#contact").click(function(){
     $("#jumbo").fadeOut("slow");
     $("#form").fadeIn("slow");
